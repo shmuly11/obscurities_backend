2b6ab20c058efe5cac63335e60ebf526449b02aa
shmuly11 <shmuly11@gmail.com>
2021-03-09 18:03:24 -0500

    favorite route

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,16 @@ class FavoritesController < ApplicationController
 
     def create
         favorite = Favorite.create(favorite_params)
-        render json: favorite
+        user = favorite.favoriter
+        render json: user
     end
 
     def destroy
-        favorite = Favorite.find(params[:id])
+        favorite = Favorite.find_by(favorite_params)
+        user = favorite.favoriter
+        
         favorite.delete
+        render json: user
     end
 
     def favorite_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'users#login'
   post '/signup', to: 'users#signup'
   get '/hobbies', to: 'hobbies#index'
+  delete '/favorites', to: 'favorites#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
