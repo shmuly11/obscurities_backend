aa973ea9eb89c8eb15cdce040e802e78b88a5e4a
shmuly11 <shmuly11@gmail.com>
2021-03-10 17:56:38 -0500

    passwword authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
 
     def login
         user = User.find_by(username: user_params[:username])
-        render json: user
+        if user && user.authenticate(params[:password])
+            
+            # token = JWT.encode({ user_id: user.id }, 'my$ecretK3y', 'HS256')
+            # render json: { user: UserSerializer.new(user), token: token }
+            render json: user
+          else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+          end
+        
     end
 
     def signup
